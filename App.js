ae544e9108b3e41496497ba1d0069889828fb44a
Artem Gilmudinov <guliash92@gmail.com>
2019-10-09 22:06:22 +0300

    translate without matrices;

--- App.js
+++ App.js
@@ -4,7 +4,6 @@ import {
   View,
   PanResponder
 } from 'react-native';
-import MatrixMath from 'react-native/Libraries/Utilities/MatrixMath';
 
 const VIEW_WIDTH = 100;
 const VIEW_HEIGHT = 100;
@@ -19,14 +18,16 @@ class App extends React.Component {
       containerHeight: null
     };
 
-    this.translate = false;
-    this.prevTranslateLocation = null;
+    this.isTranslating = false;
+    this.prevTouchPageX = null;
+    this.prevTouchPageY = null;
 
+    this.x = null;
+    this.y = null;
+
+    this.isScaling = false;
+    this.scale = 1;
     this.scalePrevDistance = null;
-    
-    this.transformMatrix = MatrixMath.createIdentityMatrix();
-    this.translateMatrix = MatrixMath.createIdentityMatrix();
-    this.scaleMatrix = MatrixMath.createIdentityMatrix();
 
     this.panResponder = PanResponder.create({
       onStartShouldSetPanResponder: () => {
@@ -81,72 +82,53 @@ class App extends React.Component {
   }
 
   enableTranslate(event) {
-    this.translate = true;
+    this.isTranslating = true;
     const touch = event.nativeEvent.touches[0];
-    this.prevTranslateLocation = { x: touch.pageX, y: touch.pageY };
+    this.prevTouchPageX = touch.pageX;
+    this.prevTouchPageY = touch.pageY;
   }
 
   disableTranslate() {
-    this.translate = false;
-    this.prevTranslateLocation = null;
+    this.isTranslating = false;
+    this.prevTouchPageX = null;
+    this.prevTouchPageY = null;
   }
 
   enableScale(event) {
-    this.scale = true;
+    this.isScaling = true;
     const touch1 = event.nativeEvent.touches[0];
     const touch2 = event.nativeEvent.touches[1];
     this.scalePrevDistance = this.dist(touch1, touch2);
   }
 
   disableScale() {
-    this.scale = false;
+    this.isScaling = false;
     this.scalePrevDistance = null;
   }
 
   handleTranslate(event) {
-    if (!this.translate) {
+    if (!this.isTranslating) {
       return;
     }
+
     const touch = event.nativeEvent.touches[0];
-    
-    const diffX = touch.pageX - this.prevTranslateLocation.x;
-    const diffY = touch.pageY - this.prevTranslateLocation.y;
 
-    this.transformMatrix = this.translatePost(diffX, diffY);
+    const diffX = touch.pageX - this.prevTouchPageX;
+    const diffY = touch.pageY - this.prevTouchPageY;
 
-    this.transformView.setNativeProps({ style: { transform: [ { matrix: this.transformMatrix } ] } });
-    this.prevTranslateLocation = { x: touch.pageX, y: touch.pageY };
+    this.x += diffX;
+    this.y += diffY;
+
+    this.transformView.setNativeProps(
+      { style: { transform: [ { translateX: this.x }, { translateY: this.y } ] } }
+    );
+
+    this.prevTouchPageX = touch.pageX;
+    this.prevTouchPageY = touch.pageY;
   }
 
   handleScale(event) {
-    if (!this.scale) {
-      return;
-    }
-    
-    const touch1 = event.nativeEvent.touches[0];
-    const touch2 = event.nativeEvent.touches[1];
-    const midX = (touch1.pageX + touch2.pageX) / 2;
-    const midY = (touch1.pageY + touch2.pageY) / 2;
-    const dist = this.dist(touch1, touch2);
-    const scaleFactor = dist / this.scalePrevDistance;
-
-    let result = MatrixMath.createIdentityMatrix();
-    MatrixMath.reuseTranslate2dCommand(this.translateMatrix, -midX, -midY);
-    MatrixMath.multiplyInto(result, this.transformMatrix, this.translateMatrix);
-    this.transformMatrix = result;
-
-    result = MatrixMath.createIdentityMatrix();
-    MatrixMath.reuseScaleCommand(this.scaleMatrix, scaleFactor);
-    MatrixMath.multiplyInto(result, this.scaleMatrix, this.transformMatrix);
-    this.transformMatrix = result;
-
-    result = MatrixMath.createIdentityMatrix();
-    MatrixMath.reuseTranslate2dCommand(this.translateMatrix, midX, midY);
-    MatrixMath.multiplyInto(result, this.transformMatrix, this.translateMatrix);
-    this.transformMatrix = result;
-
-    this.transformView.setNativeProps({ style: { transform: [ { matrix: this.transformMatrix } ] } });
-    this.scalePrevDistance = dist;
+   
   }
 
   dist(touch1, touch2) {
@@ -156,24 +138,25 @@ class App extends React.Component {
     );
   }
 
-  translatePost(diffX, diffY) {
-    const result = MatrixMath.createIdentityMatrix();
-    MatrixMath.reuseTranslate2dCommand(this.translateMatrix, diffX, diffY);
-    MatrixMath.multiplyInto(result, this.translateMatrix, this.transformMatrix);
-    return result;
-  }
-
   render() {
     return (
       <View
         style={ styles.container } { ...this.panResponder.panHandlers }
         onLayout={
-          event => this.setState({
-            containerWidth: event.nativeEvent.layout.width,
-            containerHeight: event.nativeEvent.layout.height
-          })
+          event => {
+            const width = event.nativeEvent.layout.width;
+            const height = event.nativeEvent.layout.height;
+            this.setState({
+              containerWidth: width,
+              containerHeight: height
+            });
+            this.x = 0;
+            this.y = 0;
+          }
         }>
-        <View style={ styles.box } ref={ it => this.transformView = it } />
+        <View
+          style={ styles.box }
+          ref={ it => this.transformView = it } />
       </View>
     );
   }
@@ -183,8 +166,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'grey',
-    justifyContent: 'center',
-    alignItems: 'center'
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start'
   },
   box: {
     backgroundColor: 'orange',
